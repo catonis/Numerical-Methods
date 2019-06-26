1a8770e7aa2b7d90e51cfb8d2f619d4cf19b962a
catonis <41175262+catonis@users.noreply.github.com>
2019-06-25 20:26:36 -0500

    Update primes.py
    Updated function to use an Atkin Sieve to generate primes.

--- primes.py
+++ primes.py
@@ -2,20 +2,33 @@
 """
 Created on Mon May 20 02:33:19 2019
 
-@author: cdm18
+@author: Chris Mitchell
 """
 
-def yieldPrimes():
-    """A generator that yields prime numbers"""
+def getPrimes(breakInt = 10 ** 9):
 
-    yield 2
+    from math import sqrt, ceil, floor
     
-    primeCandidate = 3
-    isPrime = True
+    primeList = [2, 3, 5]
+    sieve = [False] * (breakInt + 1)
     
-    while primeCandidate:
-        for divisor in range(2, primeCandidate // 2):
-            if primeCandidate % divisor == 0: isPrime = False
-        if isPrime: yield primeCandidate
-        primeCandidate += 2
-        isPrime = True
+    for x in range(1, ceil(sqrt(breakInt)) + 1):
+        for y in range(1, ceil(sqrt(breakInt)) + 1):
+            n = 4 * x ** 2 + y ** 2
+            if n <= breakInt and (n % 12 == 1 or n % 12 == 5):
+                sieve[n] = not sieve[n]
+            n = 3 * x ** 2 + y ** 2
+            if n <= breakInt and n % 12 == 7:
+                sieve[n] = not sieve[n]
+            n = 3 * x ** 2 - y ** 2
+            if x > y and n <= breakInt and n % 12 == 11:
+                sieve[n] = not sieve[n]
+    
+    for x in range(7, floor(sqrt(breakInt))):
+        if sieve[x]:
+            for y in range(x ** 2, breakInt + 1 , x ** 2):
+                sieve[y] = False
+    for i in range(7, breakInt):
+        if sieve[i]:
+            primeList.append(i)
+    return primeList

394bde43c7ee7bb679e3ba87908f472453774c80
catonis <41175262+catonis@users.noreply.github.com>
2019-07-03 01:01:40 -0500

    Update primes.py
    Updated comments to getPrimes and added the function yeildPrimes.

--- primes.py
+++ primes.py
@@ -5,7 +5,33 @@ Created on Mon May 20 02:33:19 2019
 @author: Chris Mitchell
 """
 
+def yieldPrimes():
+    """A generator that yields prime numbers"""
+
+    yield 2
+    
+    primeCandidate = 3
+    isPrime = True
+    
+    while primeCandidate:
+        for divisor in range(2, primeCandidate // 2):
+            if primeCandidate % divisor == 0: isPrime = False
+        if isPrime: yield primeCandidate
+        primeCandidate += 2
+        isPrime = True
+
+
 def getPrimes(breakInt = 10 ** 9):
+    """This function implements the Atkin-Bernstein sieve. The function
+    itself takes an integer value which is the upper boundary for the
+    search of primes. It returns all the primes from 2 to the boundary,
+    which defaults to one billion.
+    
+    The sieve first assumes that all integers up to the boundary value
+    are not prime. It then pushes the prime numbers through the "sieve"
+    These are determined by examining the properties of solutions sets
+    to three equations:  4x^2 + y^2 = n, 3x^2 + y^2 = n, and 3x^2 - y^2 = n.
+    """
 
     from math import sqrt, ceil, floor
     

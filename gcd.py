905b64effece667883d764e226039d86f338a59f
catonis <41175262+catonis@users.noreply.github.com>
2019-07-15 02:58:15 -0500

    Update gcd.py
    Simplified the logic in gcd. Added a function for finding the lcm of two numbers and wrote two functions to handle multiple numbers for each function, gcdx and lcmx.

--- gcd.py
+++ gcd.py
@@ -6,11 +6,41 @@ Created on Wed May 22 05:33:15 2019
 """
 
 def gcd(x, y):
-    """Find the greatest common denominator betweeen x and y."""
+    """Find the greatest common denominator betweeen x and y,
+    using Euclid's algorithm."""
     
     if x == y: return x
 
-    while True:
-        if x < y: x, y = y, x
-        x %= y
-        if x == 0: return y
+    while y: x, y = y, x % y
+    return x
+
+
+def gcdx(*args):
+    """Find the greatest common denominator between all given numbers."""
+
+    n = len(args) - 1
+    j = args[n]
+    for i in range(n - 1, -1, -1):
+        j = gcd(args[i], j)
+
+    return j
+        
+        
+def lcm(x, y):
+    """Find the least common multiple of x and y."""
+    
+    return (x * y) // gcd(x, y)
+
+
+def lcmx(*args):
+    """Find the least common multiple between all given numbers."""
+
+    n = len(args) - 1
+    j = args[n]
+    for i in range(n - 1, -1, -1):
+        j = (args[i] * j) // gcd(args[i], j)
+
+    return j
+
+
+    

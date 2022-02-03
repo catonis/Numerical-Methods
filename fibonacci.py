a400baea5e01c3e224847a7ad3e3935e6320e36d
Chris Mitchell <41175262+catonis@users.noreply.github.com>
2022-02-03 06:27:19 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,26 @@
+#!C:\Users\cdm18\anaconda3\python.exe
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jun 25 02:01:44 2019
 
 @author: Chris Mitchell
+
+A Fibonacci sequence generator.
 """
 
-def Fibonacci(breakInt):
-    
-    fibList = [0, 1]
-    for i in range(2, breakInt):
-        newFibNum = fibList[-1] + fibList[-2]
-        fibList.append(newFibNum)
-        if newFibNum > 4 * 10 ** 16: break
-    return fibList
+def yieldFibonacci():
+    """
+    A simple iterator to yield the Fibonacci sequence, starting at 1.
+
+    Yields
+    ------
+    int
+        The next number in the Fibonacci sequence.
+
+    """
+    yield 1
+    a = 1
+    b = 2
+    while True:
+        yield b
+        a, b = b, a + b
